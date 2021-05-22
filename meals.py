9f208f57ae9a1b19020b2e5b90f95e57187237cb
lucas <lucas.lambda.101@gmail.com>
2021-05-21 23:28:17 -0300

    Update app to deploy

--- meals.py
+++ meals.py
@@ -1,13 +1,17 @@
-import flask
+from flask import Flask, render_template, url_for
 import meal_functions
 
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return flask.render_template('index.html', list_meals=meal_functions.list_meals())
+    return render_template('index.html', list_meals=meal_functions.list_meals())
 
-@app.route("/id")
-def meal_instructions():
-    return flask.render_template('meal_instructions')
+@app.route("/recipe/<int:id>", methods=['GET'])
+def meal_instructions(id):
+    meal = meal_functions.get_meal_by_id(id)
+    return render_template('meal_instructions.html', meal=meal)
+
+if __name__ == "__main__":
+   app.run(host='0.0.0.0')
