5d5591889e7044085a8533fe06608457bbe8374a
snlucas <lucas.lambda.101@gmail.com>
2021-06-08 23:06:41 -0300

    Refact to new model design, and decrease requests

--- application.py
+++ application.py
@@ -1,23 +1,35 @@
-from flask import Flask, render_template, url_for
-import meal_functions
+from app.controller.meal_factory import MealFactory
+from flask import Flask, render_template
 import string
 
 
 application = Flask(__name__)
+dict_meals  = MealFactory()  # Reusable var
 
+
+# Route to the first page, and items to the pagination
 @application.route("/")
-@application.route("/<letter>", methods=["GET"])
-def index(letter="a"):
-    letter = letter.lower()
-    letters = string.ascii_letters
-    previous_letter = "" if letter == "a" else letters[letters.index(letter) - 1]
-    next_letter = "" if letter == "z" else letters[letters.index(letter) + 1]
-    return render_template("index.html", list_meals=meal_functions.list_meals(), letter=letter, previous_letter=previous_letter, next_letter=next_letter)
+def index():
+    letter = "a"
+    return render_template("index.html", list_meals=dict_meals.alphabetic_meals, letter=letter, previous_letter="", next_letter="c")
 
+
+@application.route("/letter/<letter>", methods=["GET"])
+def page_letter(letter):
+    # Set the pagination
+    letter = letter.lower()
+    letters = string.ascii_letters  # alphabet letters
+    previous_letter = "" if letter == "a" else letters[letters.index(letter) - 1]  # first item of the pagination
+    next_letter = "" if letter == "z" else letters[letters.index(letter) + 1]  # last item in the pagination
+    return render_template("index.html", list_meals=dict_meals.alphabetic_meals, letter=letter, previous_letter=previous_letter, next_letter=next_letter)
+
+
+# Show recipe details of an id meal
 @application.route("/recipe/<int:id>", methods=["GET"])
 def meal_instructions(id):
-    meal = meal_functions.get_meal_by_id(id)
+    meal = dict_meals.search_id_meals_dict(id)
     return render_template("meal_instructions.html", meal=meal)
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
