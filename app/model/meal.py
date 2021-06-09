aa13c754c872b2219523a66dda3778591cddfee4
snlucas <lucas.lambda.101@gmail.com>
2021-06-08 23:10:45 -0300

    create model class

---
+++ app/model/meal.py
@@ -0,0 +1,9 @@
+class Meal():
+    def __init__(self, id, name, category, area, thumbnail, instructions):
+        self.id = id
+        self.name = name
+        self.category = category
+        self.area = area
+        self.thumbnail = thumbnail
+        self.instructions = instructions
+        
